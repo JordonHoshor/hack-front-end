54ca70d10b8a807a9365f3aed64655067b6d2bce
JordonHoshor <Jordon.Hoshor@gmail.com>
2016-11-19 19:12:33 -0700

    rendered data to page

--- components/main/main.controller.js
+++ components/main/main.controller.js
@@ -11,8 +11,45 @@
   function mainController(mainService) {
     /*jshint validthis: true */
     this.greeting = 'The Alexa Hustle';
+    this.players = {};
+    this.players.bench = [];
     mainService.getPlayers().then((data) => {
-      console.log(data);
+      const results = data.data.data;
+      console.log(results);
+      results.map(player => {
+        switch (player.team_slot) {
+          case 'QB':
+            this.players.QB = player;
+            break;
+          case 'RB1':
+            this.players.RB1 = player;
+            break;
+          case 'RB2':
+            this.players.RB2 = player;
+            break;
+          case 'WR1':
+            this.players.WR1 = player;
+            break;
+          case 'WR2':
+            this.players.WR2 = player;
+            break;
+          case 'TE':
+            this.players.TE = player;
+            break;
+          case 'FLEX':
+            this.players.FLEX = player;
+            break;
+          case 'DEF':
+            this.players.DEF = player;
+            break;
+          case 'K':
+            this.players.K = player;
+            break;
+          case 'Bench':
+            this.players.bench.push(player);
+            break;
+        }
+      })
     });
   }
 
