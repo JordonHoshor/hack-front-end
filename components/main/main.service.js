718fdf9dfac1820148e23ded4a9bd74b19b922da
JordonHoshor <Jordon.Hoshor@gmail.com>
2016-11-19 18:25:30 -0700

    wired up

--- components/main/main.service.js
+++ components/main/main.service.js
@@ -8,9 +8,13 @@
 
   mainService.$inject = ['$http'];
 
+  const baseUrl = 'https://broncoshack.herokuapp.com/';
+
   function mainService($http) {
     /*jshint validthis: true */
-    console.log('mainService');
+    this.getPlayers = () => {
+      return $http.get(baseUrl);
+    };
   }
 
 })();
